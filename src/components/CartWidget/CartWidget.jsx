07c23792926f46b27c59b51845ed9f8ea1dd1975
gonzal86 <gonzaloh.latierro@gmail.com>
2022-08-24 20:31:04 -0300

    Actualizacion del nabvar + cartWidget

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./CartWidget.css"
+
+export default function CartWidget() {
+    return (
+        <div>
+            <img src="https://st4.depositphotos.com/15827064/19737/v/450/depositphotos_197370110-stock-illustration-shopping-cart-icon-commerce-icon.jpg" width="50" heigth="50"></img>
+        </div>
+    )
+}
